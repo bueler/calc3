133a5f10c9e5710a262208312a240b006992c31f
Ed Bueler <elbueler@alaska.edu>
2023-03-03 07:46:23 -0900

    make results agree better Matlab and Octave

--- assets/worksheets/S23/contourfocus.m
+++ assets/worksheets/S23/contourfocus.m
@@ -4,8 +4,9 @@ f = @(x,y) 2*x.^2 + x.*y + 5 * y.^2 + 2 * x;
 ezcontour(f,[-1 0 -0.5 0.5])
 hold on
 plot(-20/39,2/39,'ko')
-text(-20/39 + 0.02,2/39,'(-20/39,2/39)','fontsize',18)
+text(-20/39 + 0.02,2/39,'(-20/39,2/39)','fontsize',14)
 ezcontour(f,[-0.7 -0.3 -0.1 0.2])
+axis([-1 0 -0.5 0.5])
 xlabel x, ylabel y
 hold off
  
